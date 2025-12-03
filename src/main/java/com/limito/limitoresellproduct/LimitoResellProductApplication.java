e380d7530bc1ad007388ef42d2329bbf4e03b168
jo-hseo <ellejo1110@gmail.com>
2025-12-03 16:00:12 +0900

    chore (#4) : git에 추가 안 된 파일 추가

---
+++ src/main/java/com/limito/limitoresellproduct/LimitoResellProductApplication.java
@@ -0,0 +1,13 @@
+package com.limito.limitoresellproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.limito")
+public class LimitoResellProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitoResellProductApplication.class, args);
+	}
+
+}
