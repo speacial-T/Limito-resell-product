d074282a47e99323c6085e0ae295a8a96be7ca82
jo-hseo <ellejo1110@gmail.com>
2025-12-05 23:58:55 +0900

    feat (#3) :  리셀 상품 생성 도메인 repository 인터페이스 추가

---
+++ src/main/java/com/limito/limitoresellproduct/domain/repository/ResellProductRepository.java
@@ -0,0 +1,8 @@
+package com.limito.limitoresellproduct.domain.repository;
+
+import com.limito.limitoresellproduct.domain.model.Product;
+
+public interface ResellProductRepository {
+
+	Product save(Product product);
+}
