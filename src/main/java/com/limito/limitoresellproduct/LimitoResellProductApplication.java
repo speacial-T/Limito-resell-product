13e76b31ef2fc39f7c74561c1a65adca187d2b0a
jo-hseo <ellejo1110@gmail.com>
2025-12-05 13:07:42 +0900

    feat (#4) : 공통 모듈 패키지 빈 수동 스캔 설정 제거

--- src/main/java/com/limito/limitoresellproduct/LimitoResellProductApplication.java
+++ src/main/java/com/limito/limitoresellproduct/LimitoResellProductApplication.java
@@ -3,7 +3,7 @@ package com.limito.limitoresellproduct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.limito")
+@SpringBootApplication
 public class LimitoResellProductApplication {
 
 	public static void main(String[] args) {
