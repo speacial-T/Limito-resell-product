8a2fa0b14dfe26da3394ac13f001ff30818c8c67
jo-hseo <ellejo1110@gmail.com>
2025-12-06 20:57:28 +0900

    feat (#3) : toEntity 추가

--- src/main/java/com/limito/limitoresellproduct/application/service/ResellProductService.java
+++ src/main/java/com/limito/limitoresellproduct/application/service/ResellProductService.java
@@ -1,12 +1,9 @@
 package com.limito.limitoresellproduct.application.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.limito.limitoresellproduct.domain.model.Product;
 import com.limito.limitoresellproduct.domain.repository.ResellProductRepository;
-import com.limito.limitoresellproduct.domain.vo.Option;
 import com.limito.limitoresellproduct.infrastructure.persistence.mapper.ProductMapper;
 import com.limito.limitoresellproduct.presentation.dto.request.ProductCreateRequestV1;
 import com.limito.limitoresellproduct.presentation.dto.response.ProductCreateResponseV1;
@@ -21,23 +18,7 @@ public class ResellProductService {
 	private final ResellProductRepository resellProductRepository;
 
 	public ProductCreateResponseV1 createProduct(@Valid ProductCreateRequestV1 request) {
-		List<Option> options = request.getOptions().stream()
-			.map(optionRequest -> new Option(
-				null,
-				optionRequest.modelNumber(),
-				optionRequest.size(),
-				optionRequest.color(),
-				optionRequest.thumbnailUrl(),
-				optionRequest.details(),
-				null)
-			)
-			.toList();
-		Product product = new Product(
-			request.getProductName(),
-			request.getBrandName(),
-			request.getCategoryId(),
-			options
-		);
+		Product product = ProductMapper.toEntity(request);
 		Product savedProduct = resellProductRepository.saveProduct(product);
 		return ProductMapper.toDto(savedProduct);
 	}
--- src/main/java/com/limito/limitoresellproduct/infrastructure/persistence/mapper/ProductMapper.java
+++ src/main/java/com/limito/limitoresellproduct/infrastructure/persistence/mapper/ProductMapper.java
@@ -1,13 +1,40 @@
 package com.limito.limitoresellproduct.infrastructure.persistence.mapper;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.limito.limitoresellproduct.domain.model.Product;
 import com.limito.limitoresellproduct.domain.vo.MinimumPriceStock;
+import com.limito.limitoresellproduct.domain.vo.Option;
+import com.limito.limitoresellproduct.presentation.dto.request.ProductCreateRequestV1;
 import com.limito.limitoresellproduct.presentation.dto.response.ProductCreateResponseV1;
+
+import jakarta.validation.Valid;
 
 @Component
 public class ProductMapper {
+
+	public static Product toEntity(@Valid ProductCreateRequestV1 request) {
+		List<Option> options = request.getOptions().stream()
+			.map(optionRequest -> new Option(
+				null,
+				optionRequest.modelNumber(),
+				optionRequest.size(),
+				optionRequest.color(),
+				optionRequest.thumbnailUrl(),
+				optionRequest.details(),
+				null)
+			)
+			.toList();
+		Product product = new Product(
+			request.getProductName(),
+			request.getBrandName(),
+			request.getCategoryId(),
+			options
+		);
+		return product;
+	}
 
 	public static ProductCreateResponseV1 toDto(Product product) {
 		return ProductCreateResponseV1.builder()
