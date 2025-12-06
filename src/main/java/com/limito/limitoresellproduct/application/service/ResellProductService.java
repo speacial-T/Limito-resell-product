e89c0592600917ae43f65616596fe1a4628b88bc
jo-hseo <ellejo1110@gmail.com>
2025-12-06 17:25:34 +0900

    fix (#3) : DTO, 레포지토리 수정에 따른 서비스 수정

--- src/main/java/com/limito/limitoresellproduct/application/service/ResellProductService.java
+++ src/main/java/com/limito/limitoresellproduct/application/service/ResellProductService.java
@@ -1,9 +1,13 @@
 package com.limito.limitoresellproduct.application.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.limito.limitoresellproduct.domain.model.Product;
 import com.limito.limitoresellproduct.domain.repository.ResellProductRepository;
+import com.limito.limitoresellproduct.domain.vo.Option;
+import com.limito.limitoresellproduct.infrastructure.persistence.mapper.ProductMapper;
 import com.limito.limitoresellproduct.presentation.dto.request.ProductCreateRequestV1;
 import com.limito.limitoresellproduct.presentation.dto.response.ProductCreateResponseV1;
 
@@ -17,13 +21,24 @@ public class ResellProductService {
 	private final ResellProductRepository resellProductRepository;
 
 	public ProductCreateResponseV1 createProduct(@Valid ProductCreateRequestV1 request) {
+		List<Option> options = request.getOptions().stream()
+			.map(optionRequest -> new Option(
+				null,
+				optionRequest.modelNumber(),
+				optionRequest.size(),
+				optionRequest.color(),
+				optionRequest.thumbnailUrl(),
+				optionRequest.details(),
+				null)
+			)
+			.toList();
 		Product product = new Product(
 			request.getProductName(),
 			request.getBrandName(),
 			request.getCategoryId(),
-			request.getOptions()
+			options
 		);
-		Product savedProduct = resellProductRepository.save(product);
-		return savedProduct.toDto();
+		Product savedProduct = resellProductRepository.saveProduct(product);
+		return ProductMapper.toDto(savedProduct);
 	}
 }
