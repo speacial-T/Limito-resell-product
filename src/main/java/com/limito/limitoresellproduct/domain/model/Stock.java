735cd3ebe5c6bb3122147521601782336a084bf0
jo-hseo <ellejo1110@gmail.com>
2025-12-07 10:42:09 +0900

    feat (#8) : 재고 도메인 모델 및 도메인 로직 구현

--- src/main/java/com/limito/limitoresellproduct/domain/model/Stock.java
+++ src/main/java/com/limito/limitoresellproduct/domain/model/Stock.java
@@ -1,6 +1,11 @@
 package com.limito.limitoresellproduct.domain.model;
 
+import java.util.Optional;
 import java.util.UUID;
+
+import com.limito.common.audit.UserContextHolder;
+import com.limito.common.exception.AppException;
+import com.limito.limitoresellproduct.presentation.advice.ProductErrorCode;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,13 +13,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Min;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "p_resell_product_stocks")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Stock {
 
 	@Id
@@ -25,12 +32,49 @@ public class Stock {
 	private UUID optionId;
 
 	@Column(name = "price", nullable = false)
-	@Size(min = 0)
+	@Min(value = 0)
 	private int price;
 
 	@Column(name = "is_deleted", nullable = false)
 	private boolean isDeleted = false;
 
 	@Column(name = "seller_id", nullable = false)
-	private UUID sellerId;
+	private Long sellerId;
+
+	public Stock(UUID optionId, int price) {
+		setOptionId(optionId);
+		setPrice(price);
+		setSellerId();
+	}
+
+	private void setOptionId(UUID optionId) {
+		if (optionId == null) {
+			throw new AppException(
+				ProductErrorCode.INVALID_DOMAIN_INFO.getStatus(),
+				ProductErrorCode.INVALID_DOMAIN_INFO.getMessage() + ": 옵션ID는 필수 입력값입니다."
+			);
+		}
+		this.optionId = optionId;
+	}
+
+	private void setPrice(int price) {
+		if (price < 0) {
+			throw new AppException(
+				ProductErrorCode.INVALID_DOMAIN_INFO.getStatus(),
+				ProductErrorCode.INVALID_DOMAIN_INFO.getMessage() + ": 가격은 0이상 이어야 합니다."
+			);
+		}
+		this.price = price;
+	}
+
+	private void setSellerId() {
+		Optional<Long> userId = UserContextHolder.getCurrentUserId();
+		if (userId.isEmpty()) {
+			throw new AppException(
+				ProductErrorCode.INVALID_DOMAIN_INFO.getStatus(),
+				ProductErrorCode.INVALID_DOMAIN_INFO.getMessage() + ": 판매자를 알 수 없습니다."
+			);
+		}
+		this.sellerId = userId.get();
+	}
 }
