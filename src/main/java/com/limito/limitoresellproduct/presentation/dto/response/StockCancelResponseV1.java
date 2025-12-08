7317bd66f8ead14c41f4b8e6362fc1e783969e59
jo-hseo <ellejo1110@gmail.com>
2025-12-08 22:18:04 +0900

    feat (#18) : 재고 예약 취소 응답 DTO 추가

---
+++ src/main/java/com/limito/limitoresellproduct/presentation/dto/response/StockCancelResponseV1.java
@@ -0,0 +1,20 @@
+package com.limito.limitoresellproduct.presentation.dto.response;
+
+import java.util.List;
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Builder
+public class StockCancelResponseV1 implements InternalResponse {
+
+	private String errorCode;
+
+	private String message;
+
+	@Setter
+	private List<UUID> stockIds;
+}
