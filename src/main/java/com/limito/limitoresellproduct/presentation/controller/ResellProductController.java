7793d20ae6b56a829d9d94edcf94e7724d871f77
jo-hseo <ellejo1110@gmail.com>
2025-12-08 20:14:23 +0900

    feat (#17) : 머지 충돌 해결하면서 지워진 import 추가

--- src/main/java/com/limito/limitoresellproduct/presentation/controller/ResellProductController.java
+++ src/main/java/com/limito/limitoresellproduct/presentation/controller/ResellProductController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
