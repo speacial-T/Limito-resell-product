345253a52dd50abf52b40f6d21fa8f0ee8765f85
jo-hseo <ellejo1110@gmail.com>
2025-12-08 20:11:51 +0900

    feat (#17) : 재고 리스트 도메인 모델 추가 및 최저가 재고 계산 로직 구현

---
+++ src/main/java/com/limito/limitoresellproduct/domain/model/Stocks.java
@@ -0,0 +1,16 @@
+package com.limito.limitoresellproduct.domain.model;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Stocks {
+	private List<Stock> stocks;
+
+	public static Stock calculateMinStock(List<Stock> stocks) {
+		Stock minStock = stocks.stream()
+			.min(Comparator.comparing(Stock::getPrice))
+			.orElse(null);
+
+		return minStock;
+	}
+}
