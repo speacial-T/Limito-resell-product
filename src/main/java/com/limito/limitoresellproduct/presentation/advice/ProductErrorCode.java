b699f3e44f8f6c1dfc1e8d2e22dc5ae7ec792370
jo-hseo <ellejo1110@gmail.com>
2025-12-10 15:49:27 +0900

    refactor (checkstyle) : checkstyle 수정

--- src/main/java/com/limito/limitoresellproduct/presentation/advice/ProductErrorCode.java
+++ src/main/java/com/limito/limitoresellproduct/presentation/advice/ProductErrorCode.java
@@ -12,8 +12,7 @@ import lombok.Getter;
 public enum ProductErrorCode implements ErrorCode {
 	INVALID_DOMAIN_INFO(HttpStatus.BAD_REQUEST, "잘못된 도메인 정보입니다."),
 	WRONG_ID(HttpStatus.BAD_REQUEST, "E001 : 잘못된 리셀 상품 재고 ID입니다."),
-	OUT_OF_STOCK(HttpStatus.BAD_REQUEST, "E002 : 재고 부족"),
-	;
+	OUT_OF_STOCK(HttpStatus.BAD_REQUEST, "E002 : 재고 부족");
 
 	private HttpStatus status;
 	private String message;
