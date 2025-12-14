bb695c9322d44c8a54a97eddc8cc7456abe7fa70
jo-hseo <ellejo1110@gmail.com>
2025-12-14 13:44:52 +0900

    refactor (#38) : 불필요한 validation 제거

--- src/main/java/com/limito/limitoresellproduct/presentation/dto/response/ProductCreateResponseV1.java
+++ src/main/java/com/limito/limitoresellproduct/presentation/dto/response/ProductCreateResponseV1.java
@@ -3,28 +3,16 @@ package com.limito.limitoresellproduct.presentation.dto.response;
 import java.util.List;
 import java.util.UUID;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 public class ProductCreateResponseV1 {
-
-	@NotNull
 	private UUID productId;
-
-	@NotBlank
 	private String productName;
-
-	@NotBlank
 	private String brandName;
-
-	@NotNull
 	private UUID categoryId;
-
-	@NotNull
 	private List<OptionResponseV1> options;
 
 	public record OptionResponseV1(
