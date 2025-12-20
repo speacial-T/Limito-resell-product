9f91f4badb15de19ebfa19c77a8e977a00c203f8
jo-hseo <ellejo1110@gmail.com>
2025-12-20 21:21:46 +0900

    fix (#45) : 에러코드 추가

--- src/main/java/com/limito/limitoresellproduct/presentation/advice/ProductErrorCode.java
+++ src/main/java/com/limito/limitoresellproduct/presentation/advice/ProductErrorCode.java
@@ -18,6 +18,7 @@ public enum ProductErrorCode implements ErrorCode {
 	INACTIVE_PRODUCT(HttpStatus.BAD_REQUEST, "비활성화된 상품입니다."),
 	INACTIVE_OPTION(HttpStatus.BAD_REQUEST, "비활성화된 옵션입니다."),
 	INACTIVE_STOCK(HttpStatus.BAD_REQUEST, "비활성화된 재고입니다."),
+	INACTIVE_MODEL(HttpStatus.BAD_REQUEST, "비활성화된 모델입니다."),
 	ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 재고가 존재합니다.");
 
 	private HttpStatus status;
