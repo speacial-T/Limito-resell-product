9406ecb3016945581f11f804891b5406d984c47e
jo-hseo <ellejo1110@gmail.com>
2025-12-21 04:43:38 +0900

    feat (#46) : 장바구니->상품 정보 요청 - 서비스 메서드 추가

--- src/main/java/com/limito/limitoresellproduct/application/service/ProductStockService.java
+++ src/main/java/com/limito/limitoresellproduct/application/service/ProductStockService.java
@@ -1,6 +1,7 @@
 package com.limito.limitoresellproduct.application.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
@@ -11,8 +12,10 @@ import com.limito.limitoresellproduct.domain.repository.ResellModelRepository;
 import com.limito.limitoresellproduct.domain.repository.ResellStockRepository;
 import com.limito.limitoresellproduct.infrastructure.persistence.mapper.ProductMapper;
 import com.limito.limitoresellproduct.presentation.dto.request.ProductInfosGetRequestV1;
+import com.limito.limitoresellproduct.presentation.dto.response.OptionInfosGetResponseV1;
 import com.limito.limitoresellproduct.presentation.dto.response.ProductInfosGetResponseV1;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -28,6 +31,18 @@ public class ProductStockService {
 				Option option = model.getOption(request.getOptionId());
 				Stock stock = resellStockRepository.findByIdOrElseThrow(request.getStockId());
 				return ProductMapper.toProductInfosGetResponseV1(model, option, stock);
+			})
+			.toList();
+
+		return response;
+	}
+
+	public List<OptionInfosGetResponseV1> getOptiontInfos(@Valid List<UUID> optionIds) {
+		List<OptionInfosGetResponseV1> response = optionIds.stream()
+			.map(optionId -> {
+				Model model = resellModelRepository.findByOptionIdOrElseThrow(optionId);
+				Option option = model.getOption(optionId);
+				return ProductMapper.toOptionInfosGetResponseV1(model, option);
 			})
 			.toList();
 
