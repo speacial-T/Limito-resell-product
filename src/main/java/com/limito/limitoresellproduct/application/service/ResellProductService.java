9c9f6854c59ccf365a262d5d3d1275c79e549de9
jo-hseo <ellejo1110@gmail.com>
2025-12-21 03:55:11 +0900

    fix (#45) : 상품 서비스 모델 조회로 변경, 메서드 배치 순서 변경

--- src/main/java/com/limito/limitoresellproduct/application/service/ResellProductService.java
+++ src/main/java/com/limito/limitoresellproduct/application/service/ResellProductService.java
@@ -7,16 +7,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.limito.common.exception.AppException;
-import com.limito.limitoresellproduct.domain.model.Product;
-import com.limito.limitoresellproduct.domain.repository.ResellProductRepository;
+import com.limito.common.security.audit.UserRole;
+import com.limito.common.security.auth.PreAuthorized;
+import com.limito.limitoresellproduct.domain.model.Model;
+import com.limito.limitoresellproduct.domain.model.Option;
+import com.limito.limitoresellproduct.domain.repository.ResellModelRepository;
 import com.limito.limitoresellproduct.domain.vo.MinimumPriceStock;
-import com.limito.limitoresellproduct.domain.vo.Option;
 import com.limito.limitoresellproduct.infrastructure.persistence.mapper.ProductMapper;
 import com.limito.limitoresellproduct.presentation.advice.ProductErrorCode;
 import com.limito.limitoresellproduct.presentation.dto.request.ProductCreateRequestV1;
 import com.limito.limitoresellproduct.presentation.dto.response.ProductCreateResponseV1;
 import com.limito.limitoresellproduct.presentation.dto.response.ProductGetResponseV1;
-import com.limito.limitoresellproduct.presentation.dto.response.ProductReadResponseV1;
+import com.limito.limitoresellproduct.presentation.dto.response.ProductsGetResponseV1;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -34,25 +36,26 @@ public class ResellProductService {
 		return ProductMapper.toProductCreateResponseV1(savedModel);
 	}
 
-	@Transactional
-	public void changeMinimumPriceStock(UUID productId, UUID optionId, MinimumPriceStock minStock) {
-		Model model = resellModelRepository.findByIdOrElseThorw(productId);
-		product.validateActive();
-		product.changeMinimumPriceStock(optionId, minStock);
-	}
-
-	public ProductsGetResponseV1 getProducts(UUID categoryId, Pageable pageable) {
-		Page<Model> models = resellProductRepository.findAllByCategoryIdForAllUser(categoryId, pageable);
+	public ProductsGetResponseV1 getModels(UUID categoryId, Pageable pageable) {
+		Page<Model> models = resellModelRepository.findAllByCategoryIdForAllUser(categoryId, pageable);
 		return ProductMapper.toProductsGetResponseV1(categoryId, models);
 	}
 
-	public ProductGetResponseV1 getProduct(UUID resellProductId) {
-		Model model = resellModelRepository.findByIdForAllUserOrElseThrow(resellProductId);
-		return ProductMapper.toProductGetResponseV1(product);
+	public ProductGetResponseV1 getModel(UUID modelId) {
+		Model model = resellModelRepository.findByIdForAllUserOrElseThrow(modelId);
+		return ProductMapper.toProductGetResponseV1(model);
 	}
 
-	public void validateProductAndOption(UUID productId, UUID optionId) {
-		Model model = resellProductRepository.findByIdOrElseThorw(productId);
+	@Transactional
+	public void changeMinimumPriceStock(UUID modelId, UUID optionId, MinimumPriceStock minStock) {
+		Model model = resellModelRepository.findByOptionIdOrElseThrow(optionId);
+		// Model model = resellModelRepository.findByIdForAllUserOrElseThrow(modelId);
+		model.validateActive();
+		model.changeMinimumPriceStock(optionId, minStock);
+	}
+
+	public void validateModelAndOption(UUID modelId, UUID optionId) {
+		Model model = resellModelRepository.findByIdForAllUserOrElseThrow(modelId);
 		model.validateActive();
 
 		Option option = model.getOption(optionId);
